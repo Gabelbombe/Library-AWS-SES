c880f786995059f95fc69ed5509d8a0afc879a28
ehime <dodomeki@gmail.com>
2015-05-22 15:11:25 -0700

    Adding in testcase, AWS SDK requires Guzzle

--- tests/index.php
+++ tests/index.php
@@ -1,1 +1,11 @@
-<?php phpinfo();
+<?php
+
+define  ('APP_HOME', getenv('APP_HOME'));
+require APP_HOME . '/vendor/autoload.php';
+
+$sns = New \Wrappers\Aws\SimpleEmailService(
+    getenv('KEY'),
+    getenv('PEM')
+);
+
+print_r($sns);
